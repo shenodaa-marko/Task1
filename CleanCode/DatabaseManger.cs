2ada276df92059ce228e9289dcb6eccbf421e3e7
shenodaa-marko <shenodamarko@gmail.com>
2023-03-03 20:54:22 +0200

    this is the clean code of the task 1

---
+++ CleanCode/DatabaseManger.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace CleanCode
+{
+    internal class DatabaseManger : IDisposable
+    {
+        #region Private Members
+
+        private const string databaseConnection = "Data Source=server;Initial Catalog=db;User ID=user;Password=password";
+
+        private readonly SqlConnection sqlConnection;
+
+        private readonly SqlCommand sqlCommand;
+        #endregion
+
+        #region Constractor And Dispser
+
+        public DatabaseManger(string sqlQuery)
+        {
+            sqlConnection = new SqlConnection(databaseConnection);
+            sqlCommand = new SqlCommand(sqlQuery, sqlConnection);
+        }
+
+        public void Dispose()
+        {
+            sqlCommand.Dispose();
+            sqlConnection.Dispose();
+        }
+
+        ~DatabaseManger()
+        {
+            Dispose();
+        }
+        #endregion
+
+        #region DataBase Commands Methods
+
+        /// <summary>
+        /// Insert new data to database based on SQL command
+        /// </summary>
+        /// <returns></returns>
+        public bool InsertData()
+        {
+            sqlConnection.Open();
+            try
+            {
+                sqlCommand.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while save data....\n" + ex.Message);
+                return false;
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        /// <summary>
+        /// Get data from database based on SQL command
+        /// </summary>
+        /// <returns></returns>
+        public SqlDataReader SelectData()
+        {
+            return sqlCommand.ExecuteReader();
+        }
+        #endregion
+    }
+}
---
+++ CleanCode/User.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace CleanCode
+{
+    /// <summary>
+    /// CleanCode Fix 
+    /// </summary>
+    public class User
+    {
+        #region Private Members
+
+        private string commandQuery;
+
+        private DatabaseManger databaseManger;
+        #endregion
+
+        #region Public Members
+        public string UserName { get; set; }
+
+        public int UserAge { get; set; }
+        #endregion
+
+        #region User database Methods
+
+        /// <summary>
+        /// Insert new user to database
+        /// </summary>
+        /// <param name="newUser">The new user object to insert to database</param>
+        /// <returns></returns>
+        public bool SaveUserToDataBase(User newUser)
+        {
+            commandQuery = $"INSERT INTO Users (Name,Age) VALUES ('{newUser.UserName}',{newUser.UserAge})";
+
+            using (databaseManger = new DatabaseManger(commandQuery))
+                return databaseManger.InsertData();
+        }
+
+        /// <summary>
+        /// Get all users form the database
+        /// </summary>
+        public void GetUsersList()
+        {
+            commandQuery = "SELECT * FROM Users";
+
+            using (databaseManger = new DatabaseManger(commandQuery))
+            using (SqlDataReader sqlDataReader = databaseManger.SelectData())
+            {
+                while (sqlDataReader.Read())
+                    Console.WriteLine(sqlDataReader["Name"] + ", " + sqlDataReader["Age"]);
+            }
+        }
+        #endregion
+
+        #region User genral Methods
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Doing something...");
+        }
+        #endregion
+    }
+}
